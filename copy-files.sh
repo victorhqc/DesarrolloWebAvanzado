ec04172b7a5f1f09a67575838430f515d6e5f300
Victor Quiroz Castro <victorhqc@gmail.com>
2019-06-16 20:09:29 +0200

    Fix bash script

--- copy-files.sh
+++ copy-files.sh
@@ -4,6 +4,7 @@ echo Copying files to XAMPP
 
 if [[ "$1" != "" && "$1" != "./copy-files.sh" && "$1" != "copy-files.sh" ]];
 then
+  rm -rf /Users/$1/.bitnami/stackman/machines/xampp/volumes/root/htdocs/tarea
   cp -R src/ /Users/$1/.bitnami/stackman/machines/xampp/volumes/root/htdocs/tarea
   echo Copied files successfully
   echo Access to http://localhost:8080/tarea
