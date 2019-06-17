736ab9fb6346866d2ae0df52d47e7fee530cebb8
Victor Quiroz Castro <victorhqc@gmail.com>
2019-06-17 22:31:19 +0200

    Redirect from logout to login

--- src/logout.php
+++ src/logout.php
@@ -3,6 +3,9 @@
 require_once(__DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php');
 
 use App\Authorization\Authorization;
+use App\Utils\Route;
 
 $authorization = new Authorization();
 $authorization->remove_authorization();
+
+Route::redirect_to_relative("login.php");
