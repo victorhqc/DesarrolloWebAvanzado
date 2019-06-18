11c886785bd8365ac23e3537be6638cb9b5b825b
Victor Quiroz Castro <victorhqc@gmail.com>
2019-06-18 19:12:05 +0200

    Fix session problem

---
+++ src/controllers/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use \Exception;
+use App\Authorization\Authorization;
+use App\Utils\Route;
+
+class IndexController {
+  private $authorization;
+
+  function __construct() {
+    $this->authorization = new Authorization();
+  }
+
+  public function handle_redirection() {
+    try {
+      if (!$this->authorization->is_authorized()) {
+        Route::redirect_to("login.php");
+      }
+
+      Route::redirect_to("products.php");
+
+    } catch (\Exception $e) {
+      Route::redirect_to("login.php");
+    }
+  }
+}
--- src/index.php
+++ src/index.php
@@ -2,19 +2,7 @@
 
 require_once(__DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php');
 
-use App\Authorization\Authorization;
-use App\Utils\Route;
+use App\Controllers\IndexController;
 
-try {
-  $authorization = new Authorization();
-  $user = $authorization->get_authorized_user();
-
-  if (!$user) {
-    Route::redirect_to("login.php");
-  }
-
-  Route::redirect_to("products.php");
-
-} catch (\Exception $e) {
-  Route::redirect_to("login.php");
-}
+$controller = new IndexController();
+$controller->handle_redirection();
