ace76ab0e2bb2734465c20311862a923d8eefcf6
Victor Quiroz Castro <victorhqc@gmail.com>
2019-08-04 13:55:30 +0200

    Display login Very basic example. No real authentication implemented yet.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es-MX" dir="ltr">
+    <head>
+        <title>Catálogo - @yield('title')</title>
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossorigin="anonymous"
+          />
+        <style type="text/css">
+            html, body {
+                height: 100%;
+            }
+        </style>
+        <script
+            src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+        <script
+            src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
+        @yield('app-styles')
+    </head>
+    <body>
+        @section('topbar')
+            This is the master topbar.
+        @show
+
+        <div>
+            @yield('content')
+        </div>
+    </body>
+</html>
