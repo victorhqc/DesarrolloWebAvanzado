38133bcc47a4e87c469885a1e03e9734c51f2499
Victor Quiroz Castro <victorhqc@gmail.com>
2019-08-04 14:36:35 +0200

    Add users migration

---
+++ database/migrations/2019_08_04_123253_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('email', 144)->unique();
+            $table->string('password', 512);
+            $table->string('name', 144);
+            $table->string('last_name', 144);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
