4a8e3069c76349f6438956740ebd08f19fc22927
Victor Quiroz Castro <victorhqc@gmail.com>
2019-08-05 19:56:18 +0200

    Default header Still need to add dynamic properties but at least now is not needed to manually add it every time.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,38 +2,6 @@
 
 @section('title', 'Inicio')
 
-<!-- TODO: Reutilizar-->
-@section('topbar')
-    <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Catálogo</a>
-    <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav" role="navigation">
-        <li>Hello World</li>
-      </ul>
-    </div>
-    <div>
-      <span class="navbar-text mr-3">
-        Test
-      </span>
-      <span class="navbar-text">
-        <a class="btn btn-outline-info" href="<?php echo url("/logout") ?>" role="button">
-          Cerrar sesión
-        </a>
-      </span>
-    </div>
-  </nav>
-@endsection
-
 @section('content')
     <div class="container mt-4">
       <div class="jumbotron">
