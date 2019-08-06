357ccaca73cdb5f39e35409f43cbf8a5f60bc9bc
Victor Quiroz Castro <victorhqc@gmail.com>
2019-08-06 19:34:59 +0200

    Mover lógica al modelo del usuario: - el correo de gravatar - is_admin

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,27 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Traits\HasAdministrator;
-use App\Traits\GravatarEmail;
-
 use App\Product;
 
 class ProductsController extends Controller {
 
-    use HasAdministrator;
-    use GravatarEmail;
-
     public function showProducts(Request $request) {
-        $isAdmin = $this->isAdmin($request);
         $user = $request->user();
 
         // TODO: Se debería de implementar un páginado en algún momento.
         $products = Product::all();
 
         return view('products', [
-            'isAdmin' => $isAdmin,
+            'isAdmin' =>  isset($user) ? $user->is_admin() : false,
             'products' => $products,
-            'email_img' => $this->email_gravatar_url($request, 30),
+            'email_img' =>  isset($user) ? $user->email_gravatar_url(30) : '',
             'email' => isset($user) ? $user->email : '',
         ]);
     }
